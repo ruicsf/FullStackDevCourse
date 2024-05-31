9f8ca80003187039904c9d377da46c28ff621988
Rui Fernandes <ruicsf@gmail.com>
2024-05-31 11:47:56 -0800

    modified:   src/App.jsx

--- Part 2/phonebook/src/App.jsx
+++ Part 2/phonebook/src/App.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
-import { useState, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import axios from 'axios'
 import PersonList from './components/PersonList';
-import AddName from './components/AddName';
+import AddName from './components/AddName'; 
 import FilterInput from './components/FilterInput';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([])
+  useEffect (() => {
+    console.log('effect')
+
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      console.log('promise fullfiled')
+      setPersons(response.data) 
+    })
+  }, [])
+  console.log ('render', persons.length, 'persons')
+
+
+  // const [persons, setPersons] = useState([
+  //   { name: 'Arto Hellas', number: '040-123456', id: 1 },
+  //   { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+  //   { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+  //   { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  // ]);
 
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
